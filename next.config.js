c5a6be0c034608aa59df7dea53c8117ce13891d9
Igor Venturelli <igventurelli@gmail.com>
2026-07-08 16:44:44 -0300

    fix: path forwarding

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "protre.fit",
+          },
+        ],
+        destination: "https://promostreino.com.br/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.protre.fit",
+          },
+        ],
+        destination: "https://promostreino.com.br/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
